035e0fc290efff408d99f382fe33005256528dd1
DrDomi2 <dvidovic1@etfos.hr>
2023-10-31 17:11:59 +0100

    FirstCommit

---
+++ SomeNewApp/SomeNewApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SomeNewApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Dominik Vidović");
+        }
+    }
+}
